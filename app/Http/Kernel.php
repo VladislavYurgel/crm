64e32c8143563203d6de43ce789f8f36dc3f4e9a
Vladislav <Vladislav.Yurgel@r-stylelab.com>
2017-10-02 11:33:17 +0300

    Added CORS middleware for api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\AuthJWT;
+use App\Http\Middleware\CORS;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -58,6 +59,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'jwt' => AuthJWT::class
+        'jwt' => AuthJWT::class,
+        'cors' => CORS::class
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::group(['prefix' => 'v1'], function() {
-
-    Route::group(['namespace' => 'Api\Auth'], function() {
-        Route::post('auth/login', 'AuthController@login');
-        Route::post('auth/register', 'AuthController@register');
+Route::group(['middleware' => 'cors'], function() {
+    Route::group(['prefix' => 'v1'], function() {
+        Route::group(['namespace' => 'Api\Auth'], function() {
+            Route::post('auth/login', 'AuthController@login');
+            Route::post('auth/register', 'AuthController@register');
+        });
     });
-
 });
